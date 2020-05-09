941745f21d188e29fa212b360f19925900289820
Atik <atik.shaikh@hnrtech.com>
2020-05-09 20:06:23 +0530

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,5 @@
-import React from "react"
 import '../../App.css';
-import ProductList  from './Screen/productList';
-import ProductDetails from './Screen/productDetail'
-import Header from './Screen/header'
 import home from "./Screen/home";
 
-//export default Header;
 export default home;
-//export default home;
 
